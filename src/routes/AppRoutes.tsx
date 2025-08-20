7110c77dcbdc7118c11517a9b39e2f6ba948e6a8
unknown <lequangbaolam2001@gmail.com>
2025-08-20 09:14:05 +0700

    Fix build

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,9 @@
-import { Routes, Route } from "react-router-dom";
-// import TopPage from "../pages/TopPage;
-// import MyRecordPage from "../pages/MyRecordPage";
-// import ColumnPage from "../pages/ColumnPage";
+import { Routes } from "react-router-dom";
 
 export default function AppRoutes() {
   return (
     <main className="container" style={{ paddingTop: 24 }}>
-      <Routes>
-        {/* <Route path="/" element={<TopPage />} /> */}
-        {/* <Route path="/my-record" element={<MyRecordPage />} /> */}
-        {/* <Route path="/column" element={<ColumnPage />} /> */}
-      </Routes>
+      <Routes />
     </main>
   );
 }
